b00491dc733d89cbda2648b10c57620bdabd7d96
Gonzalo <gonzalo.patino@se.com>
2025-08-20 20:23:12 -0400

    initial release

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+
+const app = express();
+
+app.use(express.json());
+
+//Middleware to handle sessions
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req,res,next){
+//Middleware to authenticate users using JWT
+if (req.session.authorization) { //Get the authorization object stored in the session
+    token = req.session.authorization['accessToken'] //Retrieve the token from authorization object
+    jwt.verify(token, "access", (err, user)=>{ // Use JWT to verify token
+        if (!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({ message: "User not authenticated"})
+        }
+
+    })
+
+
+}
+});
+ 
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
---
+++ router/auth_users.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username)=>{ //returns boolean
+    // Filter users array for any user with the same username
+    let userswithsamename == users.filter((user)=>{ 
+        return user.username ==username;
+    });
+    // Return ture if any user with the same username is found
+    if (userswithsamename.length > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+// Fiulter the users array for any user with the same username and password
+    let validusers = users.filter((user)=>{ return (user.username === username && user.password ==password);})
+}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
