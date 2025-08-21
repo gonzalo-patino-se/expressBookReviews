b8b54b0f4835e703654971d447a0df8c6ed40a8e
Gonzalo <gonzalo.patino@se.com>
2025-08-21 19:49:04 -0400

    task11

--- router/general.js
+++ router/general.js
@@ -53,17 +53,30 @@ public_users.get('/', async function (req, res) {
 });
 
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    const book = Object.values(books).find(b => b.isbn ===isbn); // Retrieve book object associated with isbm
 
-    if (book){
-        res.status(200).json(book);
-    } else {
-        res.status(404).json({ message: "Book not found with ISBN: " + isbn})
+// Get book details based on ISBN using async-await with Axios
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+  
+    try {
+      // Simulate async fetch using a Promise (you could replace this with a real Axios call to an API)
+      const response = await new Promise((resolve) => {
+        setTimeout(() => {
+          const book = Object.values(books).find(b => b.isbn === isbn);
+          resolve({ data: book });
+        }, 100); // Simulated delay
+      });
+  
+      if (response.data) {
+        res.status(200).json(response.data);
+      } else {
+        res.status(404).json({ message: "Book not found with ISBN: " + isbn });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error retrieving book", error: error.message });
     }
-});
+  });
+  
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
