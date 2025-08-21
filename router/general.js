d67481454e1d6431fab559ec2e9c951370662a46
Gonzalo <gonzalo.patino@se.com>
2025-08-21 15:33:56 -0400

    post registration endpoint

--- router/general.js
+++ router/general.js
@@ -6,8 +6,29 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //check if both username and password are provided
+  if (username && password){
+    // Check if the user does not already exist
+    if(!isValid(username)){
+        // Add the new user to the users array
+        users.push(
+            {
+                "username": username, 
+                "password": password
+            }
+        );
+        return res.status(200).json({ messsage: "User succesfully registered. Now you can login"});
+    }else{
+        return res.status(404).json({ message: "User already exists!"});
+    }
+  }
+  //Return error if username or password is missing
+  else{
+    return res.status(404).json({ message: "Unable to register user"});
+  }
 });
 
 // Get the book list available in the shop
@@ -59,7 +80,7 @@ public_users.get('/review/:isbn',function (req, res) {
   const book = Object.values(books).find(b => b.isbn === isbn); //Find book by ISBN
 
   if (book){
-    res.status(200).json(book.reviews)
+    res.status(200).json(book.reviews);
 
   }else{
     res.status(404).json({ message: "book not found with ISBN: " + isbn});
